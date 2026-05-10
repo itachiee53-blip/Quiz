c3c69c021cd0221b15319548e9c6e1146c00d588
itachiee53-blip <itachiee53@gmail.com>
2026-05-10 16:15:56 +0700

    Add files via upload

---
+++ english.js
@@ -0,0 +1,4 @@
+const englishQuestions = [
+    {q: 'I ____ to the police station yesterday.', options: ['go', 'gone', 'went', 'going'], ans: 2}, 
+    {q: 'He is interested ____ learning law.', options: ['on', 'at', 'in', 'with'], ans: 2}
+];
